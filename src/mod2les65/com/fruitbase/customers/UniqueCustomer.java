abc34168de8054e4c74c0c4e657f615d06c89caf
Airat Muzafarov <joevegojv@gmail.com>
2023-03-23 00:59:19 +0400

    Cырой вариант решения, классы Покупателя немного исправить успел

--- src/mod2les65/com/fruitbase/customers/UniqueCustomer.java
+++ src/mod2les65/com/fruitbase/customers/UniqueCustomer.java
@@ -14,53 +14,6 @@ public class UniqueCustomer extends Customer {
 
 //    реалиация метода takeFruits отбирает только уникальные фрукты,
 //    т.е. те, которых еще нет во внутреннем массиве
-    public void takesFruits() {
-        //создаем массив уникальных фруктов хранящий уникальные фрукты
-        Fruit[] uniqueFruits = new Fruit[1];
-
-        //укажем первый эл-т массива, чтобы было с чем сравнивать
-        //первый эл-т  не может быть дублирующим
-        uniqueFruits[0] = purchases[0];
-
-        //хранит номер эл-та массива уникальных фруктов
-        int uniqueFruitIndex = 1;
-
-        boolean isUnique = true;
-
-        for (int i = 1; i < purchases.length; i++) {
-            System.out.println("\nБерем из общего : " + getPurchases()[i].getName());
-            for (int j = 0; j < uniqueFruitIndex; j++) {
-                if (purchases[j] == null)
-                    break;
-
-                System.out.println("Сравниваем с : " + uniqueFruits[j].getName());
-
-                if (getPurchases()[i].getName().equals(uniqueFruits[j].getName())) {
-                    System.out.println("Совпали : " + getPurchases()[i].getName() +
-                            " " + uniqueFruits[j].getName());
-
-                    isUnique = false;
-                    break;
-                }
-                uniqueFruits = fruitsCopyAndPlusElement(uniqueFruits);
-                uniqueFruits = addUnfreshFruitToUnlikeArray(uniqueFruits,
-                        purchases[i], uniqueFruits.length - 1);
-                uniqueFruitIndex++;
-            }
-            if (!isUnique) {
-//                uniqueFruits = fruitsCopyAndPlusElement(uniqueFruits);
-//                uniqueFruits = addUnfreshFruitToUnlikeArray(uniqueFruits,
-//                        purchases[i], uniqueFruits.length - 1);
-//                uniqueFruitIndex++;
-//            } else {
-                unlikedFruits = fruitsCopyAndPlusElement(unlikedFruits);
-                unlikedFruits = addUnfreshFruitToUnlikeArray(unlikedFruits,
-                        purchases[i], unlikedFruits.length - 1);
-            }
-        }
-        purchases = uniqueFruits;
-    }
-
     public void takeFruits() {
         Fruit[] existedFruit = new Fruit[1];
         Fruit[] uniqueFruits = new Fruit[0];
@@ -71,33 +24,33 @@ public class UniqueCustomer extends Customer {
 
                 if(indexOfPurchases == 0){
                     existedFruit[0] = purchases[0];
-                    System.out.println("Array in iskl " + Arrays.toString(existedFruit));
+//                    System.out.println("Array in iskl " + Arrays.toString(existedFruit));
                     break;
                 }
-                System.out.println("indexOfPurchases " + indexOfPurchases + ". purchase " + purchases[indexOfPurchases].getName());
-                System.out.println("indexOfExist " + indexOfExist + ". exist " + existedFruit[indexOfExist].getName());
+//                System.out.println("indexOfPurchases " + indexOfPurchases + ". purchase " + purchases[indexOfPurchases].getName());
+//                System.out.println("indexOfExist " + indexOfExist + ". exist " + existedFruit[indexOfExist].getName());
 
                 if (purchases[indexOfPurchases].getName().equals(existedFruit[indexOfExist].getName())) {
 
-                    System.out.println("match___");
+//                    System.out.println("match___");
                     if(indexOfPurchases == 1 && existedFruit[0].getName().equals(purchases[1].getName())){
-                        System.out.println("Array in iskl " + Arrays.toString(existedFruit));
-                        System.out.println("iskluchenie");
+//                        System.out.println("Array in iskl " + Arrays.toString(existedFruit));
+//                        System.out.println("iskluchenie");
                         isUnique = false;
                         continue;
                     }
 
                     System.out.println(existedFruit.length);
 
-                    System.out.println("Array before " + Arrays.toString(existedFruit));
+//                    System.out.println("Array before " + Arrays.toString(existedFruit));
                     existedFruit = fruitsCopyAndPlusElement(existedFruit);
                     existedFruit = addUnfreshFruitToUnlikeArray(existedFruit,
                             purchases[indexOfPurchases], existedFruit.length - 1);
 
                     isUnique = false;
 
-                    System.out.println(existedFruit.length);
-                    System.out.println("Array after " + Arrays.toString(existedFruit));
+//                    System.out.println(existedFruit.length);
+//                    System.out.println("Array after " + Arrays.toString(existedFruit));
                     break;
                 }
             }
@@ -106,6 +59,7 @@ public class UniqueCustomer extends Customer {
                 existedFruit = fruitsCopyAndPlusElement(existedFruit);
                 existedFruit = addUnfreshFruitToUnlikeArray(existedFruit,
                         purchases[indexOfPurchases], existedFruit.length - 1);
+
                 uniqueFruits = fruitsCopyAndPlusElement(uniqueFruits);
                 uniqueFruits = addUnfreshFruitToUnlikeArray(uniqueFruits,
                         purchases[indexOfPurchases], uniqueFruits.length - 1);
