6c6238ba95be614c12b97e43fc94efeeb45195a5
Airat Muzafarov <joevegojv@gmail.com>
2023-04-26 15:17:52 +0400

    mod6les4

--- src/mod6les4/task1/Function.java
+++
@@ -1,7 +0,0 @@
-package mod6les4.task1;
-/*Создайте интерфейс Function, который объявляет абстрактный метод evaluate.
-Метод принимает параметр int и возвращает int.
- */
-public interface Function {
-    int evaluate(int number);
-}
